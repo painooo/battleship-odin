2fe8586a406ae2cee255c5acc77c434cbefcbc0a
pyr <jzhangnv@gmail.com>
2026-04-25 19:02:01 -0700

    Added AttackMode.js

---
+++ web/src/AttackMode.js
@@ -0,0 +1,23 @@
+import { render, isWin, attackRandom } from './util.js';
+
+export default function AttackMode([ownBoard, playerOneShips], [otherBoard, playerTwoShips]) {
+  document.body.textContent = "";
+  let currentPlayer = false; // false = player, true = AI :)
+  const onAttack = () => { // Bug: For some reason player gets a head start
+    if (isWin(currentPlayer ? playerTwoShips : playerOneShips)) {
+      document.body.textContent = "";
+      const winMessage = document.createElement("h1");
+      winMessage.classList.add("winMsg");
+      winMessage.textContent = `${currentPlayer ? "Opponent" : "You"} won!`
+      document.body.appendChild(winMessage);
+    }
+    currentPlayer = !currentPlayer;
+    if (currentPlayer) {
+      attackRandom(ownBoard, onAttack);
+    }
+  }
+  const area = document.createElement("div");
+  const args = {playBoard: otherBoard, statBoard: ownBoard, onAttack};
+  render(args, "toAttack", area);
+  document.body.appendChild(area);
+}
