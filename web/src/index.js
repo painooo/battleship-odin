e4bc71ba6d2037430593869991c8605c0b04158b
pyr <jzhangnv@gmail.com>
2026-04-25 19:02:32 -0700

    Added game logic

--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,17 @@
 import "./styles.css";
+import Board from './Board.js';
+import Ship from './Ship.js';
+import BuildMode from './BuildMode.js';
+import AttackMode from './AttackMode.js';
+import { placeRandom, makeShipCopy } from './util.js';
 
-document.body.textContent = "deadbeef";
+const globalShipList = {"Dummy1": 1,"Dummy2": 2};
+
+const boardTwo = new Board();
+const playerTwoShips = placeRandom(makeShipCopy(globalShipList), boardTwo);
+
+const boardOne = new Board();
+const onSubmit = (playerOneShips) => {
+  AttackMode([boardOne, playerOneShips], [boardTwo, playerTwoShips]);
+}
+const playerOneShips = BuildMode(makeShipCopy(globalShipList),  boardOne, onSubmit);
