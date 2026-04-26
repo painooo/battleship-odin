258c0407cc27565b20637dd30429ba374c4753ea
pyr <jzhangnv@gmail.com>
2026-04-25 18:32:04 -0700

    Fixxed extra ship length bug

--- web/src/Board.js
+++ web/src/Board.js
@@ -22,18 +22,18 @@ export default class Board {
     // perhaps a util. function might help
     if (direction == "HORIZONTAL") {
       if (x + length > 9) return false;
-      for (let i = x; i <= x + length; i++) { // Entire area gets checked first
+      for (let i = x; i < x + length; i++) { // Entire area gets checked first
         if (this.board[i][y].ship != null) return false;
       }
-      for (let i = x; i <= x + length; i++) { // Then ship is placed
+      for (let i = x; i < x + length; i++) { // Then ship is placed
         this.board[i][y].ship = ship;
       }
     } else {
       if (y + length > 9) return false;
-      for (let i = y; i <= y + length; i++) { // Entire area gets checked first
+      for (let i = y; i < y + length; i++) { // Entire area gets checked first
         if (this.board[x][i].ship != null) return false;
       }
-      for (let i = y; i <= y + length; i++) { // Then ship is placed
+      for (let i = y; i < y + length; i++) { // Then ship is placed
         this.board[x][i].ship = ship;
       }
     }
